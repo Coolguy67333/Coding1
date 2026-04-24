26d7d29bc83ec7e0cfbf156566ba0048ef992153
Coolguy67333 <Kanikagoel1027@gmail.com>
2026-04-23 19:11:28 -0700

    Add files via upload

---
+++ Homework_6.py
@@ -0,0 +1,7 @@
+ch = input("Enter any character: ")
+
+
+if((ch >= 'a' and ch <= 'z') or (ch >= 'A' and ch <= 'Z')):
+    print ("Your character is a letter of the alphabet.")
+else:
+    print("Your character is not in the alphabet.")

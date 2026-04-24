9f3258b3be984e375f0f9dcaf2387bc17365211a
Coolguy67333 <Kanikagoel1027@gmail.com>
2026-04-23 19:31:21 -0700

    Add files via upload

---
+++ Activity1_7.py
@@ -0,0 +1,23 @@
+x = 5
+if (type(x) is int):
+    print("true")
+else:
+    print("false")
+
+x= 5.5
+
+if (type(x) is not float):
+    print("true")
+else:
+    print("false")
+
+x = 20
+y = 20
+
+if (x is y):
+    print("X & Y ARE THE SAME IDENTITY!")
+
+y = 30
+
+if (x is not y):
+    print("X & Y HAVE DIFFERENT IDENTITIES")

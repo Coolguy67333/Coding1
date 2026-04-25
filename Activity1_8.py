42ca16df4077c7fe3a7aec0a377c461d6d907805
Coolguy67333 <Kanikagoel1027@gmail.com>
2026-04-25 09:14:20 -0700

    Add files via upload

---
+++ Activity1_8.py
@@ -0,0 +1,16 @@
+v = 4
+w = 5
+y = 8
+x = 2
+z = 0
+z = (v+w) * x / y;
+print("Value of (V+W) * x / y is", z)
+
+
+name = "Alex"
+age = 11
+
+if name == "Alex" or name =="John" and age >= 2:
+    print("Hello! Welcome")
+else:
+    print("Goodbye!")

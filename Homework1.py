edaf0e4c4ad6b45f1e7b99e199676c6a17c47ead
Coolguy67333 <Kanikagoel1027@gmail.com>
2026-05-30 08:54:00 -0700

    Add files via upload

---
+++ Homework1.py
@@ -0,0 +1,15 @@
+import turtle
+t = turtle.Turtle()
+s = int(input("Enter the length of the side of the Square:"))
+# drawing first side
+t.forward(s)
+t.left(90)
+# drawing second side
+t.forward(s)
+t.left(90)
+# drawing third side
+t.forward(s)
+t.left(90)
+# drawing fourth side
+t.forward(s)
+t.left(90)

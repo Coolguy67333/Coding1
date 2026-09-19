b676bfbbde73151be779747c41af4d45d5d63b3e
Coolguy67333 <Kanikagoel1027@gmail.com>
2026-09-19 09:38:15 -0700

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,17 @@
+fruits = ["apple", "mango", "banana", "grapes"]
+
+fruits.append("orange")
+
+print(fruits)
+
+fruits.remove("apple")
+print(fruits)
+fruits.pop(1)
+print(fruits)
+fruits.sort()
+print(fruits)
+fruits.reverse()
+print(fruits)
+fruits.reverse()
+print(fruits)
+fruits.clear()

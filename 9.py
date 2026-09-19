5327c1b22b4af4c9676e9b8c0b27af8e8863d5ed
Coolguy67333 <Kanikagoel1027@gmail.com>
2026-09-19 09:33:37 -0700

    Update 9.py

---
+++ 9.py
@@ -0,0 +1,14 @@
+student = {"name" : "Aarav", "age":13, "grade":7}
+print(student.get("age"))
+print(student.get("school", "N/A"))
+
+student["age"] = 14
+
+student["school"] = "Sunrise Academy"
+print(student)
+
+student.pop("grade")
+print(student)
+
+student.clear()
+print(student)
